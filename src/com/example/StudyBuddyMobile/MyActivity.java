71640c846429e0e8e0a331eebd5d2d6fe0487703
jupblb <605545fze>
2014-11-30 18:03:55 +0100

    Added action bar menu.

--- src/com/example/StudyBuddyMobile/MyActivity.java
+++ src/com/example/StudyBuddyMobile/MyActivity.java
@@ -2,14 +2,32 @@ package com.example.StudyBuddyMobile;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
